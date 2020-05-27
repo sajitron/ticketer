9f4c10f1d94f7843fd402b241d395b3b4c5cda14
sajicode <sajioloye@gmail.com>
2020-05-27 07:17:28 +0100

    setup error handling functionality

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,27 @@
 import express from 'express';
+import 'express-async-errors';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/users/currentuser', (req, res) => {
-	res.send('Hi there');
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+	throw new NotFoundError();
 });
+
+app.use(errorHandler);
 
 const port = 8000;
 
