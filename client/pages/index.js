24877ea95d485973faebb2d5ce4fc9aff7a90728
sajicode <sajioloye@gmail.com>
2020-05-31 07:08:41 +0100

    build reusable API client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,7 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-	console.log(currentUser);
-	return <h1>Landing</h1>;
+	return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>;
 };
 
 // * we do not utilize the use-request hook bcos it can only be used within a React component.
@@ -12,27 +11,13 @@ const LandingPage = ({ currentUser }) => {
 // * below we fetch data during SSR
 
 // * the host IP is different between making requests from the browser & the server. kubernetes is funny yea
-LandingPage.getInitialProps = async ({ req }) => {
-	if (typeof window === 'undefined') {
-		// * we are on the server
-		const {
-			data
-		} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-			headers: req.headers // * needed by nginx
-		});
+LandingPage.getInitialProps = async (context) => {
+	// * context holds the req property which holds data like cookies and host
+	// * we are on the server
+	const { data } = await buildClient(context).get('/api/users/currentuser');
 
-		// * whatever we return here is accessible in the component above as a prop
-		return data;
-	} else {
-		// * we are on the browser
-
-		const { data } = await axios.get('/api/users/currentuser');
-
-		// * whatever we return here is accessible in the component above as a prop
-		return data;
-	}
-
-	return {};
+	// * whatever we return here is accessible in the component above as a prop
+	return data;
 };
 
 export default LandingPage;
