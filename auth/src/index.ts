28177a9b3fbad0a7e11ec1f0ed2817a3f9321160
sajicode <sajioloye@gmail.com>
2020-06-01 07:07:24 +0100

    use env for mongo uri

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,13 +5,18 @@ const start = async () => {
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY secret is required');
 	}
+
+	if (!process.env.MONGO_URI) {
+		throw new Error('MONGO_URI secret is required');
+	}
+
 	try {
-		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+		await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useCreateIndex: true
 		});
-		console.log('MongoDB connected!');
+		console.log('MongoDB connected 🏂 auth!');
 	} catch (error) {
 		console.error(error);
 	}
