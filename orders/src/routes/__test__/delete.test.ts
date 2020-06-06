8325c13567cc49c064c38a821432760f7d028b63
sajicode <sajioloye@gmail.com>
2020-06-06 18:54:56 +0100

    setup ticket cancellatio route

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/Ticket';
+import { Order, OrderStatus } from '../../models/Order';
+
+it('marks an order as cancelled', async () => {
+	const ticket = Ticket.build({
+		title: 'The Weeknd',
+		price: 87
+	});
+
+	await ticket.save();
+
+	const user = global.signin();
+
+	const { body: order } = await request(app)
+		.post('/api/orders')
+		.set('Cookie', user)
+		.send({ ticketId: ticket.id })
+		.expect(201);
+
+	// * cancel the order
+	await request(app).delete(`/api/orders/${order.id}`).set('Cookie', user).send().expect(204);
+
+	const updatedOrder = await Order.findById(order.id);
+
+	expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo('emits an order-cancelled event');
