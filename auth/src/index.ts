e930cba298e932ebd76ae28a492ba350e2622863
sajicode <sajioloye@gmail.com>
2020-07-21 00:06:55 +0100

    add startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+	console.log('Engines Off 🚀🚀');
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY secret is required');
 	}
